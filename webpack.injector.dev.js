65dc929531ac1fac5fc2461ba78a2362a24d0e78
dutchenkoOleg <dutchenko.o.wezom@gmail.com>
2019-02-28 00:37:13 +0200

    sassLoaderOptions

--- webpack.injector.dev.js
+++ webpack.injector.dev.js
@@ -20,7 +20,7 @@ const sortCssMediaQueries = require('sort-css-media-queries');
 injector.app('./src/js/app.js', './dist/js/bundle-app.js')
 	.publicPath('/dist/js/')
 	.vendors('node_modules')
-	.externals({jquery: 'jQuery'})
+	.externals({ jquery: 'jQuery' })
 	.sourcemaps(injector.isProduction ? false : 'eval-source-map')
 	.sass('./src/sass/style.scss', './dist/css/bundle-style.css')
 	.sass('./src/sass/common.scss', './dist/css/bundle-common.css')
